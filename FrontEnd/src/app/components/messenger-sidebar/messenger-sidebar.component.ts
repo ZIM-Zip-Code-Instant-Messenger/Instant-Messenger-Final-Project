fa0998f98b2cb7607c6264d19a2ad6066d4d0d13
Zachary <zachary@Zipcoders-MacBook-Pro-2.local>
2023-05-09 20:35:49 -0400

    added logic to the messenger-sidebar component that allows user to add new chat and delete cchat....added fade to icon and rearranged main page

--- FrontEnd/src/app/components/messenger-sidebar/messenger-sidebar.component.ts
+++ FrontEnd/src/app/components/messenger-sidebar/messenger-sidebar.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./messenger-sidebar.component.scss']
 })
 export class MessengerSidebarComponent {
+chats: string[] = ["Colin", "Zach", "Hiep", "Andre"]; //creating a chats array
+addChat(){
+  this.chats.push("Heip") //need an input box to define new chat
+}
 
+removeChat(chatToBeRemoved: string){
+  const index: number = this.chats.findIndex((chat: string) => chat===chatToBeRemoved);
+  this.chats.splice(index,1);
+}
+
+openTextBox(chat: string){
+
+}
 }
