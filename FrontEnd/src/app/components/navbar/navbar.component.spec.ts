7e78fa2632ff020031c911496a4b67d19c9643e3
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-05-11 15:34:50 -0400

    edited navbar spec.ts file to match git tutorial

--- FrontEnd/src/app/components/navbar/navbar.component.spec.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -6,14 +6,20 @@ describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
-  beforeEach(() => {
+  beforeEach (waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [NavbarComponent]
-    });
+    })
+    .compileComponents();
+  }));
+  
+  beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
