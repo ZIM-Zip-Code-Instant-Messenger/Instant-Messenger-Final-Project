1377327ed18efcbacb40212ab73dbb1814ae57a0
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-05-12 20:10:45 -0400

    wire up the OpenID Connect authentication logic in the app component

--- FrontEnd/src/app/components/base/base.component.ts
+++ FrontEnd/src/app/components/base/base.component.ts
@@ -2,12 +2,27 @@ import { Component } from '@angular/core';
 import { faGhost } from '@fortawesome/free-solid-svg-icons';
 import { RootLevelService } from 'src/services/root-level.service';
 import { User } from '../../interfaces';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from 'src/app/auth.config';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.scss']
 })
+
+export class AppComponent {
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+
+  private configure() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
+
 export class BaseComponent {
   faGhost = faGhost;
 
